21ff71e62af4a5db9cf11bed74d500718af186b0
forhadhossen1 <mr.forhad1030@gamil.com>
2023-11-17 23:19:14 +0600

    added admin checking system

--- index.js
+++ index.js
@@ -42,6 +42,35 @@ async function run() {
             res.send({ token });
         })
 
+        const verifyToken = (req, res, next) => {
+            console.log('inside verify token', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'forbidden access' });
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'forbidden access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
+
+        app.get('user/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({message: 'unauthorized access'})
+            };
+            const query = {email: email};
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if(user){
+                admin = user?.role === 'admin';
+            }
+            res.send({admin});
+        })
 
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
@@ -54,7 +83,7 @@ async function run() {
 
         // ...user collection 
 
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyToken, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result);
         });
